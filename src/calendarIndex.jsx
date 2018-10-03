f4329bb6bb7426ab8cf9a42e424f1672451d8129
Benedek Gagyi <gagyibenedek@yahoo.com>
2018-10-03 18:59:16 +0200

    cxs

--- src/calendarIndex.jsx
+++ src/calendarIndex.jsx
@@ -4,10 +4,19 @@ import { Provider } from 'react-redux';
 import CalendarContainer from './CalendarContainer';
 import { configureCalendarStore } from './store/configureStore';
 
-const store = configureCalendarStore();
+const initProps = {
+    dates: [
+        new Date('1991-01-01'),
+        new Date('1992-01-01'),
+        new Date('1993-01-01'),
+        new Date('1994-01-01'),
+    ],
+};
+
+const store = configureCalendarStore(initProps);
 ReactDOM.render(
     <Provider store={store}>
-        <CalendarContainer />
+        <CalendarContainer initProps={initProps} />
     </Provider>,
     document.getElementById('calendar'),
 );
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import cxs from 'cxs';
+import classNames from 'classnames';
 
 class Calendar extends Component {
     render() {
-        console.log(this.props);
+        const textStyle = classNames(
+            border,
+            this.props.state.dirty ? green : red,
+        );
         return (
             <React.Fragment>
-                <h1>dirty: {this.props.state.dirty ? 'true' : 'false'}</h1>
+                <h1>
+                    dirty:{' '}
+                    <span className={textStyle}>
+                        {this.props.state.dirty ? 'true' : 'false'}
+                    </span>
+                </h1>
                 <input
                     type="text"
                     onChange={this.props.actions.changeSetting}
                 />
-                <button onClick={this.props.actions.save}>Save</button>
+                <button type="button" onClick={this.props.actions.save}>
+                    Save
+                </button>
+                <ul>
+                    {this.props.initProps.dates.map(date => (
+                        <li key={date.getTime()}>{date.toString()}</li>
+                    ))}
+                </ul>
             </React.Fragment>
         );
     }
 }
+
+const green = cxs({
+    color: 'tomato',
+});
+const red = cxs({
+    color: 'olive',
+});
+const border = cxs({
+    border: '2px solid black',
+});
 
 export default hot(module)(Calendar);
